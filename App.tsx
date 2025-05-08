162d37b423864dd8cec5301cbc8b991ea742a00c
ning.huang <ning.huang.ridson@gmail.com>
2025-05-08 14:28:17 +0800

    chore: 移除 react-native-gesture-handler 依赖并添加 eas.json 配置文件
    移除不再使用的 react-native-gesture-handler 依赖，并添加 eas.json 配置文件以支持 EAS 构建和提交功能

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import 'react-native-gesture-handler';
+// import 'react-native-gesture-handler';
 import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
